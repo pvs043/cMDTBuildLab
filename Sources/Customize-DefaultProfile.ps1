03e7c7c03cda743e4685c44ffd13c77c03c2c0a3
pvs043 <pvs043@outlook.com>
2016-09-14 10:13:56 +0500

    Fix CRLF (delete files)

--- Sources/Customize-DefaultProfile.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-Item "Z:\Applications\Configure - Set Start Layout\Theme01.deskthemepack"
-Import-StartLayout -LayoutPath "Z:\Applications\Configure - Set Start Layout\Default_Start_Screen_Layout.bin" -MountPath $env:SystemDrive\
