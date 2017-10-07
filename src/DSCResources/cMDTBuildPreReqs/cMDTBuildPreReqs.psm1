12954d60c14baf41672b6ed36daec6db28cb13fa
pvs043 <pvs043@outlook.com>
2017-10-07 12:04:54 +0500

    Add EOL converting for Install-MicrosoftVisualCx86x64 script

--- src/DSCResources/cMDTBuildPreReqs/cMDTBuildPreReqs.psm1
+++ src/DSCResources/cMDTBuildPreReqs/cMDTBuildPreReqs.psm1
@@ -43,7 +43,7 @@ class cMDTBuildPreReqs
                     }
 
                     # Workaround for external source script(s) from GitHub - change EOL
-                    if ($file.Name -eq "CleanupBeforeSysprep") {
+                    if ($file.Name -eq "CleanupBeforeSysprep" -or $file.Name -eq "Install-MicrosoftVisualCx86x64.wsf") {
                         $script = Get-Content -Path "$($this.DownloadPath)\$($file.Folder)\$($file.File)"
                         if ($script -notlike '*`r`n*') {
                             $script.Replace('`n','`r`n')
