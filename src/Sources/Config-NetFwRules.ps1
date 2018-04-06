c7765783179faa770ae50be36172c1e37e73983c
pvs043 <pvs043@outlook.com>
2018-04-06 20:02:53 +0500

    Fix Encoding at script

--- src/Sources/Config-NetFwRules.ps1
+++ src/Sources/Config-NetFwRules.ps1
@@ -1,5 +1,5 @@
-Enable-NetFirewallRule -DisplayGroup “File and Printer Sharing” -Verbose
-Enable-NetFirewallRule -DisplayGroup “File and Printer Sharing over SMBDirect” -Verbose
-Enable-NetFirewallRule -DisplayGroup “Remote Desktop” -Verbose
-Enable-NetFirewallRule -DisplayGroup “Remote Shutdown” -Verbose
-Enable-NetFirewallRule -DisplayGroup “Windows Management Instrumentation (WMI)” -Verbose
+Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Verbose
+Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing over SMBDirect" -Verbose
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop" -Verbose
+Enable-NetFirewallRule -DisplayGroup "Remote Shutdown" -Verbose
+Enable-NetFirewallRule -DisplayGroup "Windows Management Instrumentation (WMI)" -Verbose
