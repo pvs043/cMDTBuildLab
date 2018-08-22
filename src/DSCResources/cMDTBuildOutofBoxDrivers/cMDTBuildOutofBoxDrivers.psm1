9a6696f572812a39d975f15b2fc6911ed3ec07a5
Statiek <StaticlyCharged@gmail.com>
2018-08-21 20:03:33 -0400

    corrected CaSe and removed trailling whitespace
    Corrected case for "ImportOutofBoxDrivers" from "ImportOutOfBoxDrivers" to match filenaming case. remove traililng whitespace, and switched from alias "Foreach" to "Foreach-Object"

--- src/DSCResources/cMDTBuildOutofBoxDrivers/cMDTBuildOutofBoxDrivers.psm1
+++ src/DSCResources/cMDTBuildOutofBoxDrivers/cMDTBuildOutofBoxDrivers.psm1
@@ -4,7 +4,7 @@ enum Ensure
     Present
 }
 [DscResource()]
-class cMDTBuildOutOfBoxDrivers
+class cMDTBuildOutofBoxDrivers
 {
     [DscProperty()]
     [Ensure]$Ensure = "Present"
@@ -24,7 +24,7 @@ class cMDTBuildOutOfBoxDrivers
     [void] Set()
     {
         if ($this.ensure -eq [Ensure]::Present) {
-                $this.ImportOutOfBoxDrivers()
+                $this.ImportOutofBoxDrivers()
         }
         else {
             Invoke-RemovePath -Path "$($this.path)" -PSDriveName $this.PSDriveName -PSDrivePath $this.PSDrivePath -Verbose
@@ -35,7 +35,7 @@ class cMDTBuildOutOfBoxDrivers
     {
         # Path configured to never pass a test-path, not entirely sure what the best way to handle this is
         # Considered a 'force' flag but unsure of the validity of that in a DSC Configuration
-        $present = Invoke-TestPath -Path "$($this.path)\DoesNotExist" -PSDriveName $this.PSDriveName -PSDrivePath $this.PSDrivePath 
+        $present = Invoke-TestPath -Path "$($this.path)\DoesNotExist" -PSDriveName $this.PSDriveName -PSDrivePath $this.PSDrivePath
         if ($this.Ensure -eq [Ensure]::Present) {
             return $present
         }
@@ -44,19 +44,18 @@ class cMDTBuildOutOfBoxDrivers
         }
     }
 
-    [cMDTBuildOutOfBoxDrivers] Get()
+    [cMDTBuildOutofBoxDrivers] Get()
     {
         return $this
     }
 
-    [void] ImportOutOfBoxDrivers()
+    [void] ImportOutofBoxDrivers()
     {
         Import-MDTModule
         New-PSDrive -Name $this.PSDriveName -PSProvider "MDTProvider" -Root $this.PSDrivePath -Verbose:$false
         # Loops through each of the defined subdirectories and performs driver import function
-        Get-ChildItem $this.DriverSourcePath | foreach {
+        Get-ChildItem $this.DriverSourcePath | Foreach-Object {
             Import-MDTDriver -Path $this.Path -SourcePath $_.FullName -Verbose
         }
-        
     }
 }
