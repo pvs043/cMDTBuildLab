4b03bf4f559ed014c6272934ba585ee7b055305a
Pavel Andreev <pvs043@outlook.com>
2023-04-15 15:56:25 +0500

    Fix Pester errors

---
+++ src/Tests/Test-cMDTBuildPersistentDrive.ps1
@@ -0,0 +1,17 @@
+#
+# cMDTBuildPersistentDrive Test
+#
+$PSDriveName  = "MDT001"
+$PSDrivePath  = "E:\MDTBuildLab"
+$ComputerName = "$env:computername"
+
+Configuration MDTServer
+{
+    cMDTBuildPersistentDrive DeploymentPSDrive {
+        Ensure = "Present"
+        Name = $PSDriveName
+        Path = $PSDrivePath
+        Description = "MDT Build Share"
+        NetworkPath = "\\$ComputerName\DeploymentShare$"
+    }
+}
---
+++ src/Tests/Test-cMDTBuildSelectionProfile.ps1
@@ -0,0 +1,17 @@
+#
+# cMDTBuildSelectionProfile Test
+#
+$PSDriveName = "MDT001"
+$PSDrivePath = "E:\MDTBuildLab"
+
+Configuration MDTServer
+{
+    cMDTBuildSelectionProfile Win10x64 {
+        Ensure      = "Present"
+        Name        = "Windows 10 x64"
+        Comments    = "Packages for Windows 10 x64"
+        IncludePath = "Packages\Windows 10 x64"
+        PSDriveName = $PSDriveName
+        PSDrivePath = $PSDrivePath
+    }
+}
